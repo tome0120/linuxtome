50a503dfa871f943b461d3348c45e172c9e9728d
tome <43477159+tome0120@users.noreply.github.com>
2023-02-16 23:34:12 +0000

    script primo avvio

---
+++ script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+sudo apt install openssh-server wget python3 curl git nano htop make perl gcc bison flex build-essential software-properties-common apt-transport-https ufw snap freerdp2-dev freerdp2-x11 -y
+
+sudo apt update
+
+sudo apt upgrade -y
+
+echo "	
+alias stdn='shutdown now'
+#comando abbreviato per lo spegnimento del computer
+
+alias rst='sudo reboot'
+#restart abbreviato
+
+alias clr='clear'
+#comando clear per pulire directori
+
+alias cclr='cd && clr'
+#combinazione di cd e clr per pulire la bash e tornare alla cartella principale
+
+alias ipext='curl ipinfo.io'
+#visione del proprio ip pubblico
+
+alias cbash='nano ~/.bashrc'
+#aprire questo file per modificare alias e funzioni della bash
+
+alias rbash='source ~/.bashrc'
+#ricaricare questo file per attivare modifiche fatte al file
+
+alias hms='scrcpy -m 1024'
+#connesione trammite cavo o protocollo tcpip
+# di un telefono o smartphone android
+
+alias bsis='bettersis'
+#abbreviazione bettersis in bsis 
+
+alias meteo='curl  wttr.in/verona'
+#meteo verona
+
+alias apti='apt install'
+#apt install abbreviato ">> ~/.bashrc
+
+echo "eseguire questo comando per abilitare le modifiche 'source ~/.bashrc' "
