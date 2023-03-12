d280a2d3ac1986a6986bee0c647a250e3966afba
tome <tometom0120@gmail.com>
2023-03-12 21:11:00 +0100

    portainer update con f file

--- install_portainer.sh
+++ install_portainer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+sudo docker stop portainer
+
+sudo docker rm portainer
+
 sudo docker volume create portainer_data
 
-sudo docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /srv/portainer:/data portainer/portainer
+docker pull portainer/portainer-ce:latest
+
+docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 sudo docker start portainer
